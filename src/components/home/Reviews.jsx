b068b693d3346a6621847dfd93c6ae100b958429
arsh14082002 <thesiddiqui7@gmail.com>
2024-09-27 12:52:58 +0530

    init commit

--- src/components/home/Reviews.jsx
+++ src/components/home/Reviews.jsx
@@ -128,7 +128,6 @@ const Reviews = () => {
         </div>
       </div>
 
-      {/* Video Popup */}
       {isVideoOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-75 z-50">
           <div className="relative w-[80%] h-[80%]">
